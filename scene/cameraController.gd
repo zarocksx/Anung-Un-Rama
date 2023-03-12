641ff5bfec6fd178af824a54fe0089bf2628c512
teleteub <60780@etu.he2b.be>
2023-03-12 15:49:51 +0100

    Listen to bound key events

--- scene/cameraController.gd
+++ scene/cameraController.gd
@@ -6,10 +6,23 @@ var cam_index = 0
 func _ready():
 	cameras = get_tree().get_nodes_in_group('camera');
 	cameras[cam_index].current = true
-
+	
 func _input(event):
-	if Input.is_key_pressed(KEY_Z):
+	if event.is_action_pressed("btn_a_press"):
+		pass
+
+	if event.is_action_pressed("btn_b_press"):
+		pass
+
+	if event.is_action_pressed("btn_c_press"):
+		pass
+	
+	if event.is_action_pressed("btn_d_press"):
+		pass
+	
+	if event.is_action_pressed("camera_toggle"):
 		next_cam()
+
 
 func next_cam():
 	cam_index += 1
